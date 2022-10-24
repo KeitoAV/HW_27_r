45f567f86bc727d5c69817528a916df09263be44
KeitoAV <keito_ekka@mail.ru>
2022-10-24 14:14:09 +0300

    Add HW_28

--- ads/models.py
+++ ads/models.py
@@ -1,14 +1,31 @@
 from django.db import models
 
+from users.models import User
+
 
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
 
 class Ad(models.Model):
-    name = models.CharField(max_length=150, unique=True)
-    author = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+    author = models.ForeignKey(User, verbose_name='Автор', on_delete=models.CASCADE, related_name='ads')
     price = models.PositiveIntegerField()
-    description = models.CharField(max_length=300)
-    address = models.CharField(max_length=200)
-    is_published = models.BooleanField(default=False)
+    description = models.TextField(max_length=2000)
+    is_published = models.BooleanField(default=None)
+    image = models.ImageField(upload_to='images/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
+
+    def __str__(self):
+        return self.name
--- data/csv_to_json.py
+++ data/csv_to_json.py
@@ -1,8 +1,10 @@
 import csv
 import json
 
-ADS = 'ads'
-CATEGORY = 'categories'
+AD = 'ad'
+CATEGORY = 'category'
+LOCATION = 'location'
+USER = 'user'
 
 
 def convert_file(csv_file, json_file, model):
@@ -14,6 +16,10 @@ def convert_file(csv_file, json_file, model):
                 del row['id']
             else:
                 del row['Id']
+
+            if 'location_id' in row:
+                row['location'] = [int(row['location_id'])]
+                del row['location_id']
 
             if 'is_published' in row:
                 if row['is_published'] == 'TRUE':
@@ -30,5 +36,7 @@ def convert_file(csv_file, json_file, model):
         json_f.write(json.dumps(result, ensure_ascii=False))
 
 
-convert_file(f"{ADS}.csv", f"{ADS}.json", 'ads.ad')
+convert_file(f"{AD}.csv", f"{AD}.json", 'ads.ad')
 convert_file(f"{CATEGORY}.csv", f"{CATEGORY}.json", 'ads.category')
+convert_file(f"{LOCATION}.csv", f"{LOCATION}.json", 'users.location')
+convert_file(f"{USER}.csv", f"{USER}.json", 'users.user')
